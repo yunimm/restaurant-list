6f859bc4bfd162f0d985880b4aa92f2bb0c3acc6
yumimm <yuu9916@gmail.com>
2021-10-21 13:29:49 +0800

    fix coding style

--- app.js
+++ app.js
@@ -4,51 +4,33 @@ const port = 3000
 const app = express()
 const restaurantList = require('./restaurant.json')
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
 
-    res.render('index',{ restaurants:restaurantList.results })
-}) 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.trim().toLowerCase().includes(keyword.trim().toLowerCase()) ||
+  restaurant.category.trim().toLowerCase().includes(keyword.trim().toLowerCase())
+  })
+  if (keyword.length === 0 || keyword.includes(restaurants)) {
+    res.render('no_results')
+  } else {
+    res.render('index', { restaurants: restaurants, keyword: keyword })
+  }
+})
 
-app.get('/search',(req, res) => {
-
-    const keyword = req.query.keyword 
-    const restaurants = restaurantList.results.filter(restaurant => {
-        return restaurant.name.trim().toLowerCase().includes(keyword.trim().toLowerCase()) || 
-        restaurant.category.trim().toLowerCase().includes(keyword.trim().toLowerCase())
-    })
-
-    if (keyword.length === 0 || keyword.includes(restaurants)) {
-        res.render('no_results')
-    } else {
-        res.render('index',{ restaurants:restaurants, keyword: keyword })
-    }
-    
-}) 
-
-// app.get('/search',(req, res) => {
-
-//     const keyword = req.query.keyword 
-//     const restaurants = restaurantList.results.filter(restaurant => {
-//         return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-//     })
-//     res.render('index',{ restaurants:restaurants, keyword: keyword })
-// })
-
-
-
-app.get('/restaurants/:restaurant_id',(req, res) => {
-
-    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-    res.render('show',{ restaurant:restaurant })
-}) 
-
+app.get('/restaurants/: restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
 // setting template engine
-app.engine('handlebars',exphbs({ defaultLayout: 'main'}))
-app.set('view engine','handlebars')
-
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
 
 app.listen(port, () => {
-    console.log(`Express is running on http://localhost:${port}`)
+  console.log(`Express is running on http://localhost:${port}`)
 })
